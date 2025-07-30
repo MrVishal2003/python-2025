6743bba429f52cb1f35a626d52ad74779d613da6
MrVishal2003 <hadiyal123vvv@gmail.com>
2025-07-30 16:04:28 +0530

    today basics

---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,10 @@
+print('python is awesome!')
+
+def chai(n):
+    print(n)
+
+chai("lemon tea")
+
+chai_one = "green tea"
+chai_two = "ginger tea"
+chai_three = "masala tea"
---
+++ 01_basics/python.py
@@ -0,0 +1,3 @@
+from hello_chai import chai
+
+print('python is great!')
