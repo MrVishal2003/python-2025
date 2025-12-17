8d486ce2765c963f7069276fd869934b25b1f9c8
MrVishal2003 <hadiyal123vvv@gmail.com>
2025-12-17 14:13:54 +0530

    done today

---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,11 @@
+Fruite_color = "Red"
+Fruite = "Banana"
+
+if Fruite_color == "Green":
+    print(f"{Fruite} is unripe.")
+elif Fruite_color == "Yellow":
+    print(f"{Fruite} is ripe.")
+elif Fruite_color == "Brown":
+    print(f"{Fruite} is overripe.")
+else:
+    print("Unknown fruit color.")
---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,16 @@
+Weather = "sunny"
+
+if Weather == "sunny":
+    # print("Go for a walk!")
+    activity = "Go for a walk!"
+elif Weather == "rainy":
+    # print("Read a book indoors.")/
+    activity = "Read a book indoors."
+elif Weather == "snowy":
+    # print("Build a snowman!")
+    activity = "Build a snowman!"
+else:
+    # print("Stay inside.")
+    activity = "Stay inside."
+
+print(activity)
